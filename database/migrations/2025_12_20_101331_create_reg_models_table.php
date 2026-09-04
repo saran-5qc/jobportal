d1efb784ab080c78976df496180feb46b80951e8
Prajith P <prajithp@Prajiths-MacBook-Air.local>
2026-09-04 11:36:46 +0530

    Initial Laravel project

--- database/migrations/2025_12_20_101331_create_reg_models_table.php
+++ database/migrations/2025_12_20_101331_create_reg_models_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reg_models', function (Blueprint $table) {
             $table->id();
-              $table->string('first_name',100);
+            $table->string('first_name',100);
             $table->string('last_name',100);
             $table->string('gender');
             $table->date('date_of_birth');
